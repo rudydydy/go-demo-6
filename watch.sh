9605f529d2bfc80e8fd4d688d32aab9007e86d4e
Rudy Pangestu <rudy@sleekr.co>
2020-05-21 19:20:54 +0700

    registry changes

--- watch.sh
+++ watch.sh
@@ -2,5 +2,5 @@
 
 # watch the go files and continously deploy the service
 make linux
-skaffold run -p dev
-reflex -r "\.go$" -- bash -c 'make linux && skaffold run -p dev'
+UUID=$(uuidgen) skaffold run -p dev
+reflex -r "\.go$" -- bash -c 'make linux && UUID=$(uuidgen) skaffold run -p dev'
